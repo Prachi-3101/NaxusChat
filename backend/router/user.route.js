431bd7fe4a1dd847ca5b3638cf70e4314ca6573b
Prachi-3101 <prachistudies22@gmail.com>
2026-05-23 00:04:56 +0530

    Add authentication middleware and protect user route

--- backend/router/user.route.js
+++ backend/router/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { authUser,registerUser } from "../controllers/user.controller";
+import { authUser,registerUser } from "../controllers/user.controller.js";
+import protect from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/",registerUser);
 router.post("/login",authUser);
+
+router.get("/test",protect,(req,res) => {
+    res.json(req.user)
+})
 
 export default router;
